3532461adf5cb6d26e08acc0fd6214ddccdf9655
Willian <willianferraz405@gmail.com>
2026-03-08 09:22:14 -0300

    alterando texto

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let mensagens = [
     `Obrigado por ser essa mulher incrível, por sempre fazer de tudo por nós.
 Só tenho que agradecer por sempre estar comigo.
+faz 1 ano que nós conhecemos e que também estamos juntos, foi o melhor ano que eu poderia ter, sempre agradecendo por ter você na minha vida, faço sempre o melhor que posso para te ver bem , e feliz.
 Te amo muito meu amor ❤️.`
 ]
 
